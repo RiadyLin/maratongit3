dcf6a54899121a96c415b063d47c0539299955ea
James Riady <James Riady@JamesRiady>
2016-08-20 15:18:10 +0700

    add feature

---
+++ src/maratongit3/filebaru.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package maratongit3;
+
+/**
+ *
+ * @author James Riady
+ */
+public class filebaru {
+    
+}
